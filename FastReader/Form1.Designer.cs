0b07680d391cc10d38f8dc0486548b1b40a5aeea
kdivanovich <gtabro@abv.bg>
2014-11-14 16:59:01 +0000

    A Spritz-like application (unfinished).

---
+++ FastReader/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace FastReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisplayLabel = new System.Windows.Forms.Label();
+            this.RESET = new System.Windows.Forms.Button();
+            this.NUMS = new System.Windows.Forms.Label();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.SpeedSlowButton = new System.Windows.Forms.Button();
+            this.SpeedFastButton = new System.Windows.Forms.Button();
+            this.GoBack10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DisplayLabel
+            // 
+            this.DisplayLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.DisplayLabel.Location = new System.Drawing.Point(12, 18);
+            this.DisplayLabel.Name = "DisplayLabel";
+            this.DisplayLabel.Size = new System.Drawing.Size(232, 40);
+            this.DisplayLabel.TabIndex = 0;
+            this.DisplayLabel.Click += new System.EventHandler(this.DisplayLabel_Click);
+            // 
+            // RESET
+            // 
+            this.RESET.Location = new System.Drawing.Point(15, 161);
+            this.RESET.Name = "RESET";
+            this.RESET.Size = new System.Drawing.Size(110, 74);
+            this.RESET.TabIndex = 2;
+            this.RESET.Text = "Reset";
+            this.RESET.UseVisualStyleBackColor = true;
+            this.RESET.Click += new System.EventHandler(this.RESET_Click);
+            // 
+            // NUMS
+            // 
+            this.NUMS.BackColor = System.Drawing.SystemColors.Window;
+            this.NUMS.Location = new System.Drawing.Point(265, 18);
+            this.NUMS.Name = "NUMS";
+            this.NUMS.Size = new System.Drawing.Size(113, 217);
+            this.NUMS.TabIndex = 4;
+            this.NUMS.Click += new System.EventHandler(this.NUMS_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Location = new System.Drawing.Point(131, 69);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(113, 86);
+            this.PauseButton.TabIndex = 5;
+            this.PauseButton.Text = "Pause/ Resume";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // SpeedSlowButton
+            // 
+            this.SpeedSlowButton.Location = new System.Drawing.Point(15, 69);
+            this.SpeedSlowButton.Name = "SpeedSlowButton";
+            this.SpeedSlowButton.Size = new System.Drawing.Size(110, 40);
+            this.SpeedSlowButton.TabIndex = 7;
+            this.SpeedSlowButton.Text = "Start (500)";
+            this.SpeedSlowButton.UseVisualStyleBackColor = true;
+            this.SpeedSlowButton.Click += new System.EventHandler(this.SpeedSlowButton_Click);
+            // 
+            // SpeedFastButton
+            // 
+            this.SpeedFastButton.Location = new System.Drawing.Point(15, 115);
+            this.SpeedFastButton.Name = "SpeedFastButton";
+            this.SpeedFastButton.Size = new System.Drawing.Size(110, 40);
+            this.SpeedFastButton.TabIndex = 8;
+            this.SpeedFastButton.Text = "Start (100)";
+            this.SpeedFastButton.UseVisualStyleBackColor = true;
+            this.SpeedFastButton.Click += new System.EventHandler(this.SpeedFastButton_Click);
+            // 
+            // GoBack10
+            // 
+            this.GoBack10.Location = new System.Drawing.Point(131, 161);
+            this.GoBack10.Name = "GoBack10";
+            this.GoBack10.Size = new System.Drawing.Size(113, 32);
+            this.GoBack10.TabIndex = 9;
+            this.GoBack10.Text = "Go back 10 words";
+            this.GoBack10.UseVisualStyleBackColor = true;
+            this.GoBack10.Click += new System.EventHandler(this.GoBack10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(392, 244);
+            this.Controls.Add(this.GoBack10);
+            this.Controls.Add(this.SpeedFastButton);
+            this.Controls.Add(this.SpeedSlowButton);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.NUMS);
+            this.Controls.Add(this.RESET);
+            this.Controls.Add(this.DisplayLabel);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DisplayLabel;
+        private System.Windows.Forms.Button RESET;
+        private System.Windows.Forms.Label NUMS;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Button SpeedSlowButton;
+        private System.Windows.Forms.Button SpeedFastButton;
+        private System.Windows.Forms.Button GoBack10;
+    }
+}
+
---
+++ FastReader/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace FastReader
+{
+    public partial class Form1 : Form
+    {
+
+        Timer tmr = new Timer();
+        int currentWord;
+        bool reading = false;   
+        bool paused = false;        // checker if it's in a paused state - at start it's not
+        
+        public Form1()
+        {
+            InitializeComponent();
+            //tmr.Interval = 200;                               // set one fixed speed of reading
+            tmr.Tick += new System.EventHandler(wordChange);    // needs to be here otherwise if in the Start button it creates an event
+                                                                // handler in the event handler and it accumulates words to display            
+        }
+
+
+        private void SpeedSlowButton_Click(object sender, EventArgs e)
+        {
+            tmr.Interval = 500;
+            tmr.Start();
+            if (reading == false)       // if it's not reading yet, start it and set it to true...
+            {
+                currentWord = 0;
+                reading = true;         // ...here
+            }   
+        }
+
+        private void SpeedFastButton_Click(object sender, EventArgs e)
+        {
+            tmr.Interval = 100;         // same with higher speed
+            tmr.Start();
+            if (reading == false)       
+            {
+                currentWord = 0;
+                reading = true;
+            }  
+        } 
+
+        
+        private void wordChange(object sender, EventArgs e)
+        {            
+                showWord();           
+        }
+
+
+        private void showWord()
+        {
+            string src = "In Dark Souls, each of your character's eight statistics determines your overall level. Instead of being limited to the RPG norm of level 99, you can gain upwards of 700 levels. You can level up at Bonfires and spend accumulated Souls that improve your abilities.";
+
+            //string src = null;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("TestFile.txt"))
+                {
+                    src = sr.ReadToEnd();
+                    Console.WriteLine(src);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+
+            if (src == null)        // Display an error if the text file cannot be found - code problem error. 
+            {
+                DisplayLabel.Text = "Error: No text to be read.";
+            }
+            else
+            {
+
+                string[] book = src.Split(' ');
+
+
+                if (currentWord < book.Length)      // this is needed otherwise it deletes the last element on the screen
+                    DisplayLabel.Text = "                                   |  \n";
+
+                if (currentWord < book.Length)
+                {
+                    // we have a 20-character line. we want every word to be displayed in the middle
+
+                    int whiteSpaces;
+                    if (book[currentWord].Length % 2 == 0)  // set the number of white spaces to be printed
+                    {
+                        whiteSpaces = 35 - (book[currentWord].Length / 2);
+                    }
+                    else
+                        whiteSpaces = 34 - (book[currentWord].Length / 2);
+
+                    for (int i = 0; i < whiteSpaces; i++)
+                    {
+                        DisplayLabel.Text += " ";           // print spaces = 35 or 34 - half the word size                    
+                    }
+                    DisplayLabel.Text += book[currentWord];
+
+                    //NUMS.Text += book[currentWord] + " " + ( book[currentWord].Length/2) + " " ;   // test screen
+                    NUMS.Text += book[currentWord] + " ";
+                    currentWord += 1;
+                }
+                else
+                    reading = false;    // if it has ended reading, you can start again without resetting
+
+                DisplayLabel.Text += "\n                                   |  ";
+            }
+        }
+
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            if (reading == true && paused == false)
+            {
+                tmr.Stop();
+                paused = true;
+            }
+
+            else if (paused)    // formerly in a separate Resume button
+            {
+                tmr.Start();
+                paused = false;
+            }
+        }
+
+                
+        private void RESET_Click(object sender, EventArgs e)
+        { 
+            //if (reading == true)      // uncommenting those will have the Reset not work upon finishing the text
+            //{                              
+                DisplayLabel.Text = "";
+                NUMS.Text = "";
+                tmr.Stop();                
+                reading = false;  
+            //}
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void GoBack10_Click(object sender, EventArgs e)
+        {
+            if (currentWord >= 10)
+            {
+                currentWord -= 10;
+                NUMS.Text += " || ";
+            }
+        }
+
+        private void NUMS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisplayLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+                             
+
+    }
+}
